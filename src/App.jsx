769c32d88d6509ceca6df37f6be3c8118d1c5774
nika robakidze <nika70252@gmail.com>
2025-06-08 14:32:07 +0400

    colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
-  return (
-    <>
+  const [color, setColor] = useState("white")
 
+  const colors = ["red", "blue", "green", "yellow", "purple", "orange", "black"];
+
+  let mapped = colors.map(item => (
+    <button style={{backgroundColor: item, color: "gray"}} onClick={() => changeColor(item)}>{item}</button>
+  ))
+
+  function changeColor(prop){
+    setColor(prop)
+  }
+  
+  return(
+    <>
+    {mapped}
+    <div style={{width: "300px", height: "300px", backgroundColor: color}}></div>
     </>
   )
 }
